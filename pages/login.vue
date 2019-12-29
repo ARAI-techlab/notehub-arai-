35b3cb752e4320b1206fa5448488922d3508e298
Masaki-Fujiwara <rightprecious0301@gmail.com>
2019-12-29 16:20:41 +0900

    ちょい環境構成間違っていたので修正

---
+++ pages/login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <button @click="login">login</button>
+    <p v-if="userData!=null">ログイン中</p>
+    <p v-else>ログアウト中</p>
+    <p>{{userData}}</P>
+  </div>
+</template>
+
+<script>
+import firebase from '@/plugins/firebase'
+
+export default {
+    data(){
+        return{userData:''}
+    },
+  methods: {
+    login: function(){
+        firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    },
+    logout: function(){
+        firebase.auth().signOut();
+    },
+  },
+  mounted: function() {
+    firebase.auth().onAuthStateChanged(user => {
+      console.log(user);
+      if (user) {
+        this.userData = user;
+      } else {
+        this.userData = null;
+      };
+    });
+  },
+}
+</script>
