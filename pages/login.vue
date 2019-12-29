6ebb4ae926b18e305d9d16abb8cf0a10dd0dc888
Masaki-Fujiwara <rightprecious0301@gmail.com>
2019-12-29 17:09:16 +0900

    index.vueに飛ばす機能はいったん消した。いろいろと解説を付けておいた。

--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="container">
-    <button @click="login">login</button>
+    
+    <!--  ログインボタン。今のところはgoogleのみのログイン -->
+    <button @click="login">login</button> 
+     
+    <!--  ログアウトボタン。本来は、このページにログアウトボタンは必要ないが、開発中はあると便利なのでつけておく -->
     <button @click="logout">logout</button>
+
+    <!--  変数：userDataの中身がnullではなければ、ログイン中であるということなので（詳しくはscriptの方で解説）
+    「ログイン中」と出るようにしている。 -->
     <p v-if="userData!=null">ログイン中</p>
-    <p v-else>ログアウト中</p>
+
+    <!--  変数：userDataの中身がnullならば、ログインしていない状態であるということなので
+    「ログインしてないよ」と出るようにしている。 -->
+    <p v-else>ログインしてないよ</p>
+    
+    <!--  変数：userDataの中身を表示する。
+    ログイン中であれば、ログインユーザに関する情報やログインステータスに関する情報がぎっしりと表示される。
+    本来は必要ないが、開発中の勉強の足しになると思うので、表示するようにしている。 -->
     <p>{{userData}}</P>
   </div>
 </template>
@@ -16,20 +30,28 @@ export default {
         return{userData:''}
     },
   methods: {
+    //↓ログインボタンが押された時の処理。firebaseの機能でグーグル認証に連携する。
     login: function(){
         firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
     },
+
+    //↓ログインボタンが押された時の処理。firebaseの機能でグーグル認証に連携する。
     logout: function(){
         firebase.auth().signOut();
     },
   },
+
+  //mountedは、ページ生成時に発動させたい関数を用意するところ。
   mounted: function() {
+    //↓現在ログイン中のユーザのステータスを取得する関数。
+    //ログイン状態であれば、userに値が入っているため、if(user)の結果はtrueとなりif(user)以降の処理に進む。
+    //逆にログインしていない状態であれば、userに値が入っていないため、if(user)はfalseとなり、elseの処理の方へ進む。
     firebase.auth().onAuthStateChanged(user => {
-      console.log(user);
       if (user) {
+        //この処理では、↑の方にあるdata(){}で用意している変数、userDataに取得したユーザのステータス"user"を入れる
         this.userData = user;
-        location.href = "/";
       } else {
+        //この処理では、↑の方にあるdata(){}で用意している変数、userDataにnullを入れる。
         this.userData = null;
       };
     });
